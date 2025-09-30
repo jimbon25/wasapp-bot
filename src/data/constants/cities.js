5c90a45a7eeba55d9c3fcc646a3037282403d994
j1mb <jimbonluis90@gmail.com>
2025-09-30 14:19:44 +0700

    chore: Setup data folder structure with examples and constants

---
+++ src/data/constants/cities.js
@@ -0,0 +1,16 @@
+export const cities = {
+    "jakarta": { code: "1301", name: "Jakarta" },
+    "surabaya": { code: "1638", name: "Surabaya" },
+    "bandung": { code: "1219", name: "Bandung" },
+    "medan": { code: "1215", name: "Medan" },
+    "semarang": { code: "1435", name: "Semarang" },
+    "yogyakarta": { code: "1222", name: "Yogyakarta" },
+    "malang": { code: "1636", name: "Malang" },
+    "makassar": { code: "1508", name: "Makassar" },
+    "denpasar": { code: "1117", name: "Denpasar" },
+    "palembang": { code: "1601", name: "Palembang" },
+    "tangerang": { code: "1609", name: "Tangerang" },
+    "depok": { code: "1302", name: "Depok" },
+    "bekasi": { code: "1303", name: "Bekasi" },
+    "bogor": { code: "1304", name: "Bogor" }
+};
